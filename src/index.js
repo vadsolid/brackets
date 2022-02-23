de5303ca0e6162ce338be23098540c829acfb29e
vadsolid <maguscode@protonmail.com>
2022-02-22 21:36:50 -0500

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let  bracket = {};
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    bracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  } 
+
+  stack.push(str.charAt(0));
+  
+  for (let i = 1; i < str.length; i++) {
+    if (str.charAt(i) === bracket[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str.charAt(i)); 
+    }
+  }
+  if (stack.length > 0) {return false;}
+  return true;
 }
